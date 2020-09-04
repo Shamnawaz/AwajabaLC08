edec4e965ef3b79f99ff9a37d1ce67453e72c46d
Shamnawaz <shamnawaz.subakhan.sio@gmail.com>
2020-09-04 15:09:07 +0200

    Classe Repas.java 3

--- Repas.java
+++ Repas.java
@@ -5,6 +5,11 @@ import java.time.LocalDate;
 public class Repas {
 	
 	private int numero;
-	private LocalDate date; 
+	private LocalDate date;
+	public Repas(int numero, LocalDate date) {
+		super();
+		this.numero = numero;
+		this.date = date;
+	} 
 
 }
